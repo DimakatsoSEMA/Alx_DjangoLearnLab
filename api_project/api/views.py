2a3564831361a6bc991407a47c7e6da9d9335ab6
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-01 11:19:12 +0200

    added urls.py views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,20 @@
 from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
+
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
