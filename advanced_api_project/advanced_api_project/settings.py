71554cbe994aa8d9adb005b86cd6fb4326c7e70a
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-08 10:17:26 +0200

    readded the project

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-smcyec31*aue@zm9@8j0p_n29@(nhkerga=b6mtyn$vl^lm(7*'
+SECRET_KEY = 'django-insecure-9g)riw=^#47)(0h#*ijhn$6jj8#pubo(7ga2hjya1^hzywq(^8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
