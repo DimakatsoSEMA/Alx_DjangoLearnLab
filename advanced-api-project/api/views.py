8870fcce4560a4d23764475f80b27cd11db38454
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-08 12:25:19 +0200

    added views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,38 +1,23 @@
-from rest_framework import generics, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-# List & Create books
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-    # Filters, Search, Ordering
-    filter_backends = [
-        DjangoFilterBackend,
-        filters.SearchFilter,
-        filters.OrderingFilter
-    ]
-    filterset_fields = ['title', 'author', 'publication_year']
-    search_fields = ['title', 'author']
-    ordering_fields = ['publication_year']
-
-# Retrieve single book
-class BookRetrieveAPIView(generics.RetrieveAPIView):
+class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-# Update book
-class BookUpdateAPIView(generics.UpdateAPIView):
+class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-# Delete book
-class BookDeleteAPIView(generics.DestroyAPIView):
+class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
