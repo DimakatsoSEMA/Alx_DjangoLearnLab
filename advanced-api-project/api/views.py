fc399e47537c53d26f9c8f610d870138f9b64a8c
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-08 11:58:20 +0200

    added views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -9,9 +9,10 @@ from django_filters import rest_framework
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]  
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]  
     filterset_fields = ['title', 'author', 'publication_year']
-    ordering_fields = ['title', 'publication_year']  
+    ordering_fields = ['title', 'publication_year']
+    search_fields = ['title', 'author__name']
 
 #Retrieve a single book by ID (accessible to anyone)
 class BookDetailView(generics.RetrieveAPIView):
