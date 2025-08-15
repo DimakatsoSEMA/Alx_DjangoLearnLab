1c5be79dbdbf7e9d7e2916fcc9efb1656bef2e31
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-15 11:48:14 +0200

    added urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('posts/<int:post_id>/comments/new/', views.add_comment, name='comment-add'),
-    path('comments/<int:comment_id>/edit/', views.edit_comment, name='comment-edit'),
-    path('comments/<int:comment_id>/delete/', views.delete_comment, name='comment-delete'),
+    path('posts/<int:pk>/comments/new/', views.add_comment, name='comment-add'),
+    path('comments/<int:pk>/update/', views.update_comment, name='comment-update'),
+    path('comments/<int:pk>/delete/', views.delete_comment, name='comment-delete'),
 ]
