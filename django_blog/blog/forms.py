699e7fb8f9ea4d1ca73877a25d29b54e8c34b0af
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-15 12:38:21 +0200

    added tag widget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -12,5 +13,5 @@ class CommentForm(forms.ModelForm):
         fields = ['content']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add your comment here...'})
-            'tags': forms.TextInput(attrs={'placeholder': 'Add tags separated by commas'})
+            'tags': TagWidget(),
         }
