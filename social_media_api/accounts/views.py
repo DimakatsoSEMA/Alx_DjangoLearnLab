29cce6cf70dfa422f631c60c778530e36707e622
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-22 11:23:27 +0200

    updated views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,7 +1,10 @@
-from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, status
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.views import APIView
 from .serializers import RegisterSerializer, LoginSerializer
+from .serializers import PublicUserSerializer
 
 class RegisterView(APIView):
     def post(self, request):
@@ -18,3 +21,36 @@ class LoginView(APIView):
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+User = get_user_model()
+
+class FollowUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        if request.user.id == user_id:
+            return Response({"detail": "You cannot follow yourself."},
+                            status=status.HTTP_400_BAD_REQUEST)
+        target = get_object_or_404(User, id=user_id)
+        request.user.following.add(target)
+        return Response({
+            "detail": f"You are now following {target.username}.",
+            "you": PublicUserSerializer(request.user).data,
+            "target": PublicUserSerializer(target).data
+        }, status=status.HTTP_200_OK)
+
+
+class UnfollowUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        if request.user.id == user_id:
+            return Response({"detail": "You cannot unfollow yourself."},
+                            status=status.HTTP_400_BAD_REQUEST)
+        target = get_object_or_404(User, id=user_id)
+        request.user.following.remove(target)
+        return Response({
+            "detail": f"You unfollowed {target.username}.",
+            "you": PublicUserSerializer(request.user).data,
+            "target": PublicUserSerializer(target).data
+        }, status=status.HTTP_200_OK)
